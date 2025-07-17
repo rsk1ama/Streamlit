9eb5495a71f651b827a023236ae6250b15b561f9
rsk1ama <Korawit.srirat@th.bosch.com>
2025-07-17 10:46:01 +0700

    Update code.py

--- code.py
+++ code.py
@@ -89,7 +89,7 @@ fig.update_layout(
     
 st.plotly_chart(fig,use_container_width=True)
 with st.expander("ดูข้อมูล"):
-    st.dataframe(sensor_data,use_container_width=True)
+        st.dataframe(sensor_data,use_container_width=True)
 
 
 

5b4eb235b063286d5226a58293495db277897366
rsk1ama <Korawit.srirat@th.bosch.com>
2025-07-17 13:43:07 +0700

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Dashboard monitor anodizing", layout="wide")
 df = pd.read_excel("HistoryData.xlsx")
 df.columns = df.columns.str.strip()
 df["TIMESTAMP"] = pd.to_datetime(df["TIMESTAMP"], errors='coerce', infer_datetime_format = True)
-df.replace([0.65505,65535],np.nan, inplace = True)
+df.replace([0,65505,65535],np.nan, inplace = True)
        
 sensor_thresholds = {
         
